da60a674e003b7579e2878eea6a90392ce894378
akash23-ai <akashbhardwaj2024@gmail.com>
2023-12-07 11:19:02 +0530

    Changed the css of slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Component from "./Component";
 import picOfMe from "../assets/picofme.png";
 import { Link, useLocation } from "react-router-dom";
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,13 +6,13 @@ function Slider() {
 
     const {state} = useContext(UserContext) 
   return (
-    <div className={`${state? 'flex':'hidden'} relative sm:hidden w-full min-h-full bg-white overflow-hidden shadow-sm rounded-md  z-[10]`}>
-             <div className='relative w-full h-full top-32  text-center z-[1]'>
+    <div className={`${state? 'block':'hidden'} relative sm:hidden w-full min-h-full bg-zinc-100 overflow-hidden shadow-sm rounded-md z-[10]`}>
+             <div className='relative w-full h-full top-28  text-center z-[1]'>
                <ul className='no-underline'>
-                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right cursor-pointer text-gray-600 listItem'><Link to={""}>Home</Link></li>
-                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right cursor-pointer text-gray-600 listItem'><Link to={"/about"}>About</Link></li>
-                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right  cursor-pointer text-gray-600 listItem'><Link to={"/projects"}>Project</Link></li>
-                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right cursor-pointer text-gray-600 listItem'><Link to={"/contact"}>Contact</Link></li>
+                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right cursor-pointer text-gray-700 listItem'><Link to={""}>Home</Link></li>
+                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right cursor-pointer text-gray-700 listItem'><Link to={"/about"}>About</Link></li>
+                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right  cursor-pointer text-gray-700 listItem'><Link to={"/projects"}>Project</Link></li>
+                <li className='text-4xl font-bold hover:text-purple-700 px-12 py-10 uppercase text-right cursor-pointer text-gray-700 listItem'><Link to={"/contact"}>Contact</Link></li>
                </ul>
              </div>
         </div>
