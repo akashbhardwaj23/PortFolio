a5d6166c95cd2bcc2849273beffa721badf8519f
akash23-ai <akashbhardwaj2024@gmail.com>
2024-02-14 18:43:32 +0530

    Made some Changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button({children, isContact = false}) {
+function Button({children, isContact = false, onClick}) {
   return (
-    <button className={`btn ${isContact? "w-full": "md:w-2/4 w-auto"} py-8 px-16 text-2xl bg-indigo-600 text-white uppercase tracking-wide inline-block font-bold rounded-md text-center scale-95 `}> 
+    <button className={`btn ${isContact? "w-full": "md:w-2/4 w-auto"} py-8 px-16 text-2xl bg-indigo-600 text-white uppercase tracking-wide inline-block font-bold rounded-md text-center scale-95`} onClick={onClick}> 
         {children} 
     </button>
   )
